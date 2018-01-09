456fb8e71495b21b63f90a726727be306e3dc2c5
Özkan Bölükbaş <ozkanbolukbas@gmail.com>
2018-01-09 14:25:57 +0300

    Add queries playground file

---
+++ playground/mongoosequeries.js
@@ -0,0 +1,40 @@
+const{ObjectID}= require("mongodb")
+
+const {mongoose} = require("./../server/db/mongoose");
+const {Todo} = require("./../server/models/todo");
+const {User} = require("./../server/models/user");
+
+// var id= "5a549e03e6bc1f4108312835111";
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log("ID is not valid");
+// }
+// Todo.find({
+//   _id: id
+// }).then((todos)=>{
+//   console.log("Todos", todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo)=>{
+//   console.log("Todo", todo);
+// });
+
+// Todo.findById(id).then((todo)=>{
+//   if (!todo) {
+//     return console.log("Id not found");
+//   }
+//   console.log("Todo By Id", todo);
+// }).catch((e)=>console.log(e));
+
+var idu ="5a536e4e6841f27268baa914";
+
+User.findById(idu).then((user)=>{
+  if (!user) {
+    return console.log("Kullanıcı bulunamadı");
+  }
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e)=>{
+  console.log(e);
+});
