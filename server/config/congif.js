affd1afdac003fd142ca3eebd1261a3ba8ccd4e1
Özkan Bölükbaş <ozkanbolukbas@gmail.com>
2018-01-09 18:39:31 +0300

    spelling mistake

--- server/config/congif.js
+++
@@ -1,8 +0,0 @@
-var env=process.env.NODE_ENV || "development";
-if (env==="development") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI='mongodb://localhost/ToDoAppNew';
-} else if(env==="test"){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI='mongodb://localhost/ToDoAppTest';
-}
