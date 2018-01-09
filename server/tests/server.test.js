aba1eacd0a2d7fa2359a18793c9c289ff18870dd
Özkan Bölükbaş <ozkanbolukbas@gmail.com>
2018-01-09 16:21:55 +0300

    Test the DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -96,3 +96,38 @@ describe("GET /todos/:id", ()=>{
       .end(done);
   });
 });
+
+describe("DELETE /todos/:id", ()=>{
+  it("Should remove a todo", (done)=>{
+    var hexID = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo._id).toBe(hexID);
+      })
+      .end((err, res) =>{
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexID).then((todo)=>{
+          expect(todo).toNotExist();
+          done();
+        }).catch((e)=> done(e));
+      });
+  });
+  it("Shoul return 404 if todo not found",(done)=>{
+    request(app)
+      .delete(`/todos/${ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+  it("Should return 404 if objectID invalid", (done)=>{
+    request(app)
+      .delete(`/todos/123abc`)
+      .expect(404)
+      .end(done);
+  });
+});
